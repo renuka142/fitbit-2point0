e2beb8ab8f3a7eb96bd8efd389d870adc2859432
Renuka Sanjeev Bhure <72491412+renuka142@users.noreply.github.com>
2021-05-04 19:57:00 +0530

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,4 @@ const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
 
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
-
-// Dashboard
-router.get('/dashboard', ensureAuthenticated, (req, res) =>
-  res.render('dashboard', {
-    user: req.user
-  })
-);
-
-
-
 module.exports = router;
